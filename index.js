7cb57b2896a8683c40ffcce51d23d261416622ed
Gilberto Javier Morales Hernandez <gilberto morales@99.200-57-1.bestelclientes.com.mx>
2019-05-31 21:00:29 -0500

    adding files sample-math

---
+++ index.js
@@ -0,0 +1,9 @@
+const suma = require('./suma.js');
+const resta = require('./resta.js');
+const multiplicacion = require('multiplicacion.js');
+module.exports.suma = suma
+module.exports.resta = resta
+module.exports.multiplicacion = multiplicacion
+
+
+
---
+++ sample-math.js
@@ -0,0 +1,1 @@
+const customMath = require('./custom-math')
